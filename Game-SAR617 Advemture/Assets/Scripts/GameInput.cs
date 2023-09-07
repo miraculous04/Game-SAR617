ca2ccffc6620ed0eb152f6c752fdb17cb8372146
Seun Taiwo <seunemmanueltaiwo@gmail.com>
2023-09-07 06:34:22 +0100

    pushed

---
+++ Game-SAR617 Advemture/Assets/Scripts/GameInput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Input/Input Manager")]
+public class GameInput : ScriptableObject
+{
+    public KeyCode crouch;
+    public KeyCode jump;
+}
---
+++ Game-SAR617 Advemture/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameInput input;
+    void Start()
+    {
+        input.crouch = KeyCode.C;
+        input.jump = KeyCode.Space;
+    }
+
+    
+}
